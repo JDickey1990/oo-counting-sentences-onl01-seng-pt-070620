33d296f4266773a103224c161f64b355d5d798be
Jordan Dickey <jordan.michael.dickey@gmail.com>
2020-07-25 02:27:31 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    new_array = self.split(/[^\.!?]+[\.!?]/).map {|sentence| sentence.count| 
+   binding.pry
   end
-end
+end 
